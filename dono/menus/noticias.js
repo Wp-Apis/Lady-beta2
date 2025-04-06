c5e03a60b70fa5b42085275243dbdbdaba97e7ba
Luanzindesenvolve <164371570+Luanzindesenvolve@users.noreply.github.com>
2025-04-06 15:38:08 -0300

    Add files via upload

---
+++ dono/menus/noticias.js
@@ -0,0 +1,39 @@
+const menunot = (sender, prefix) => {
+return `༒lady bot v5༒
+⇒ @${sender.split("@")[0]}
+
+╭───────────────╮
+│     NOTICIAS
+╰───────────────╯
+╭────────────────╮
+| ೈ፝͜͡🗞️│${prefix}sbt
+| ೈ፝͜͡🗞️│${prefix}globo
+| ೈ፝͜͡🗞️│${prefix}cnnbrasil
+| ೈ፝͜͡🗞️│${prefix}uol
+| ೈ፝͜͡🗞️│${prefix}exame
+| ೈ፝͜͡🗞️│${prefix}oglobo
+| ೈ፝͜͡🗞️│${prefix}metropoles
+| ೈ፝͜͡🗞️│${prefix}folha
+| ೈ፝͜͡🗞️│${prefix}newscibernetico
+| ೈ፝͜͡🗞️│${prefix}news
+| ೈ፝͜͡🗞️│${prefix}noticia (assunto)
+| ೈ፝͜͡🗞️│${prefix}jovempan
+| ೈ፝͜͡🗞️│${prefix}agazeta
+| ೈ፝͜͡🗞️│${prefix}terra
+| ೈ፝͜͡🗞️│${prefix}estadao
+| ೈ፝͜͡🗞️│${prefix}aominuto
+| ೈ፝͜͡🗞️│${prefix}vejaabril
+| ೈ፝͜͡🗞️│${prefix}poder360
+| ೈ፝͜͡🗞️│${prefix}g1
+| ೈ፝͜͡🗞️│${prefix}vasconews
+| ೈ፝͜͡🗞️│${prefix}fut
+| ೈ፝͜͡🗞️│${prefix}fisiculturista
+| ೈ፝͜͡🗞️│${prefix}volei
+| ೈ፝͜͡🗞️│${prefix}basquetenews
+| ೈ፝͜͡🗞️│${prefix}genoticias (assunto)
+| ೈ፝͜͡🗞️│${prefix}time (nome)
+╰─────────────────
+ `
+}
+
+exports.menunot = menunot
