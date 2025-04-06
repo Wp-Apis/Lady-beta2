851fc924da59b97b594deaab7d8e035765c75ab7
Luanzindesenvolve <164371570+Luanzindesenvolve@users.noreply.github.com>
2025-04-06 15:41:05 -0300

    Add files via upload

---
+++ dono/menus/menuvip.js
@@ -0,0 +1,63 @@
+const menuvip = (sender, prefix) => {
+return `༒BEM-VINDO༒
+⇒ @${sender.split("@")[0]}
+
+『 𝐌𝐄𝐍𝐔✰𝐕𝐈𝐏 』
+╭════════════════════╯
+ | ೈ፝͜͡🤑 ${prefix}gerarcpf
+ | ೈ፝͜͡🤑 ${prefix}gerarnmr 
+ | ೈ፝͜͡🤑 ${prefix}docfake 
+ | ೈ፝͜͡🤑 ${prefix}recrutar (apenas ADM)
+ | ೈ፝͜͡🤑 ${prefix}recrutar2 (apenas ADM)
+ | ೈ፝͜͡🤑 ${prefix}ausente
+ | ೈ፝͜͡🤑 ${prefix}ativo
+ | ೈ፝͜͡🤑 ${prefix}delete
+ | ೈ፝͜͡🤑 ${prefix}dam (apaga a sua msg tbm)
+ | ೈ፝͜͡🤑 ${prefix}rgpf
+ | ೈ፝͜͡🤑 ${prefix}rgaf
+ | ೈ፝͜͡🤑 ${prefix}lerfoto
+ | ೈ፝͜͡🤑 ${prefix}getpp
+ | ೈ፝͜͡🤑 ${prefix}igstalk
+ | ೈ፝͜͡🤑 ${prefix}mediafire
+ | ೈ፝͜͡🤑 ${prefix}mediafire2
+ | ೈ፝͜͡🤑 ${prefix}s2 (figu sem legenda)
+ | ೈ፝͜͡🤑 ${prefix}chatgpt 
+ | ೈ፝͜͡🤑 ${prefix}simi
+ | ೈ፝͜͡🤑 ${prefix}lady
+ | ೈ፝͜͡🤑 ${prefix}menu+18 (no PV)
+╰════════════════════╮
+『 𝐏𝐔𝐗𝐀𝐃𝐀𝐒 』
+╭════════════════════╯
+  | ೈ፝͜͡🤑 ${prefix}nome
+ | ೈ፝͜͡🤑 ${prefix}nome2
+ | ೈ፝͜͡🤑 ${prefix}telefone
+ | ೈ፝͜͡🤑 ${prefix}telefone2
+ | ೈ፝͜͡🤑 ${prefix}gerarcpff
+ | ೈ፝͜͡🤑 ${prefix}gerarpessoa
+ | ೈ፝͜͡🤑 ${prefix}gerarsenha
+ | ೈ፝͜͡🤑 ${prefix}cpf
+ | ೈ፝͜͡🤑 ${prefix}cpf2
+ | ೈ፝͜͡🤑 ${prefix}cpf3
+ | ೈ፝͜͡🤑 ${prefix}cpf4
+ | ೈ፝͜͡🤑 ${prefix}cpf5
+ | ೈ፝͜͡🤑 ${prefix}cpf6
+ | ೈ፝͜͡🤑 ${prefix}cpf7
+ | ೈ፝͜͡🤑 ${prefix}operadora
+ | ೈ፝͜͡🤑 ${prefix}operadora2 
+ | ೈ፝͜͡🤑 ${prefix}operadora3
+ | ೈ፝͜͡🤑 ${prefix}placa
+ | ೈ፝͜͡🤑 ${prefix}placa2
+ | ೈ፝͜͡🤑 ${prefix}ddd
+ | ೈ፝͜͡🤑 ${prefix}ddd2
+ | ೈ፝͜͡🤑 ${prefix}ip
+ | ೈ፝͜͡🤑 ${prefix}ip2
+ | ೈ፝͜͡🤑 ${prefix}cep
+ | ೈ፝͜͡🤑 ${prefix}cep2
+ | ೈ፝͜͡🤑 ${prefix}cnpj 
+ | ೈ፝͜͡🤑 ${prefix}site
+ | ೈ፝͜͡🤑 ${prefix}socore
+ | ೈ፝͜͡🤑 ${prefix}parentes
+╰════════════════════╮`
+}
+
+exports.menuvip = menuvip
