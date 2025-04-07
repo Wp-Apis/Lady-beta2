8e1df7ee4b8e18df923c81e45710f4a1610a0126
Luanzindesenvolve <164371570+Luanzindesenvolve@users.noreply.github.com>
2025-04-07 20:43:51 -0300

    Add files via upload

---
+++ dono/menus/entretenimento.js
@@ -0,0 +1,140 @@
+const menuent = (sender, prefix) => {
+return `༒BEM-VINDO༒
+⇒ @${sender.split("@")[0]}
+╭───────────────╮
+│📺𝐂𝐎𝐍𝐓𝐀𝐒 𝐆𝐑𝐀𝐓𝐈𝐒🆓
+╰───────────────╯
+ ╭════════════════════╮
+ | ೈ፝͜͡🎭│${prefix}contas_netflix
+ | ೈ፝͜͡🎭│${prefix}contas_hbo
+ | ೈ፝͜͡🎭│${prefix}contas_nba
+ | ೈ፝͜͡🎭│${prefix}contas_disney+
+ | ೈ፝͜͡🎭│${prefix}contas_primevideo
+ | ೈ፝͜͡🎭│${prefix}contas_clarotv
+ | ೈ፝͜͡🎭│${prefix}contas_paramount+
+ | ೈ፝͜͡🎭│${prefix}contas_star+
+ | ೈ፝͜͡🎭│${prefix}contas_myfamily
+ | ೈ፝͜͡🎭│${prefix}contas_iptv
+ | ೈ፝͜͡🎭│${prefix}contas_dazn
+ | ೈ፝͜͡🎭│${prefix}contas_hulu
+ | ೈ፝͜͡🎭│${prefix}contas_viki
+ | ೈ፝͜͡🎭│${prefix}contas_cod
+ | ೈ፝͜͡🎭│${prefix}contas_hotmail
+ | ೈ፝͜͡🎭│${prefix}contas_outlook
+ | ೈ፝͜͡🎭│${prefix}contas_steam
+ | ೈ፝͜͡🎭│${prefix}contas_xbox
+ | ೈ፝͜͡🎭│${prefix}contas_crunchyroll
+ | ೈ፝͜͡🎭│${prefix}contas_onlyfans
+ | ೈ፝͜͡🎭│${prefix}contas_spotify
+ | ೈ፝͜͡🎭│${prefix}contas_expressvpn
+ | ೈ፝͜͡🎭│${prefix}contas_nordvpn
+ | ೈ፝͜͡🎭│${prefix}contas_terra
+ | ೈ፝͜͡🎭│${prefix}contas_duolingo
+ ╰════════════════════╯
+╭───────────────╮
+│     📺𝐒𝐄𝐑𝐈𝐄𝐒📽️
+╰───────────────╯
+╭════════════════════╮
+| ೈ፝͜͡🎭│${prefix}Serie1 (Arcane)
+| ೈ፝͜͡🎭│${prefix}Serie2 (Stranger Things)
+| ೈ፝͜͡🎭│${prefix}Serie3 (Soldados ou Zumbis)
+| ೈ፝͜͡🎭│${prefix}Serie4 (Senhor dos Anéis)
+| ೈ፝͜͡🎭│${prefix}Serie5 (Riverdale)
+| ೈ፝͜͡🎭│${prefix}Serie6 (A casa do dragão)
+| ೈ፝͜͡🎭│${prefix}Serie7 (Sintonia)
+| ೈ፝͜͡🎭│${prefix}Serie8 (Chucky)
+| ೈ፝͜͡🎭│${prefix}Serie9 (Flash)
+| ೈ፝͜͡🎭│${prefix}Serie10 (Game of Thrones)
+| ೈ፝͜͡🎭│${prefix}Serie11 (Legados)
+| ೈ፝͜͡🎭│${prefix}Serie12 (The Good Doctor)
+| ೈ፝͜͡🎭│${prefix}Serie13 (The Walking Dead)
+| ೈ፝͜͡🎭│${prefix}Serie14 (Peaky Blinders)
+| ೈ፝͜͡🎭│${prefix}Serie15 (Diários de um Vampiro)
+| ೈ፝͜͡🎭│${prefix}Serie16 (Gavião Arqueiro)
+| ೈ፝͜͡🎭│${prefix}Serie17 (Westworld)
+| ೈ፝͜͡🎭│${prefix}Serie18 (Lúcifer)
+| ೈ፝͜͡🎭│${prefix}Serie19 (Lobo Adolescente)
+| ೈ፝͜͡🎭│${prefix}Serie20 (La Casa de Papel)
+| ೈ፝͜͡🎭│${prefix}Serie21 (Cobra Kai)
+| ೈ፝͜͡🎭│${prefix}Serie22 (Love Victor)
+| ೈ፝͜͡🎭│${prefix}Serie23 (Vikings)
+| ೈ፝͜͡🎭│${prefix}Serie24 (Cavaleiro da Lua)
+| ೈ፝͜͡🎭│${prefix}Serie25 (Fear The Walking Dead)
+| ೈ፝͜͡🎭│${prefix}Serie26 (Rick Morty)
+| ೈ፝͜͡🎭│${prefix}Serie27 (Round 6)
+| ೈ፝͜͡🎭│${prefix}Serie28 (Carnival Row)
+| ೈ፝͜͡🎭│${prefix}Serie29 (Dark)
+| ೈ፝͜͡🎭│${prefix}Serie30 (Arqueiro)
+| ೈ፝͜͡🎭│${prefix}Serie31 (Eu Nunca)
+| ೈ፝͜͡🎭│${prefix}Serie32 (The Last Kingdom)
+| ೈ፝͜͡🎭│${prefix}Serie33 (O livro de Boba Fett)
+| ೈ፝͜͡🎭│${prefix}Serie34 (Ragnarok)
+| ೈ፝͜͡🎭│${prefix}Serie35 (OS 100)
+| ೈ፝͜͡🎭│${prefix}Serie36 (FBI)
+| ೈ፝͜͡🎭│${prefix}Serie37 (Extracurricular)
+| ೈ፝͜͡🎭│${prefix}Serie38 (Halo)
+| ೈ፝͜͡🎭│${prefix}Serie39 (Warrior Nun)
+| ೈ፝͜͡🎭│${prefix}Serie40 (Anne With)
+| ೈ፝͜͡🎭│${prefix}Serie41 (Patrulha do destino)
+| ೈ፝͜͡🎭│${prefix}Serie42 (Dinastia)
+| ೈ፝͜͡🎭│${prefix}Serie43 (Watchmen)
+| ೈ፝͜͡🎭│${prefix}Serie44 (Lovecraft Country)
+╰╰════════════════════╯
+╭───────────────╮
+│     📱𝐉𝐎𝐆𝐎𝐒💻
+╰───────────────╯
+╭════════════════════╮
+| ೈ፝͜͡🎭│${prefix}Jogo1 (Minecraft)
+| ೈ፝͜͡🎭│${prefix}Jogo2 (DragonBall Z - Dokkan)
+| ೈ፝͜͡🎭│${prefix}Jogo3 (GTA SanAndreas)
+| ೈ፝͜͡🎭│${prefix}Jogo4 (GTA SA - Legendado em Português)
+| ೈ፝͜͡🎭│${prefix}Jogo5 (GTA SA com Mod Cleo)
+| ೈ፝͜͡🎭│${prefix}Jogo6 (GTA SA Online)
+| ೈ፝͜͡🎭│${prefix}Jogo7 (GTA SuperLite + Mod Cleo)
+| ೈ፝͜͡🎭│${prefix}Jogo8 (LastDay on Eart Survival)
+| ೈ፝͜͡🎭│${prefix}Jogo9 (Bomber Friends MOD)
+| ೈ፝͜͡🎭│${prefix}Jogo10 (Arcade Hunter)
+| ೈ፝͜͡🎭│${prefix}Jogo11 (Defesa de Zombie Ocioso)
+| ೈ፝͜͡🎭│${prefix}Jogo12 (Pac-Man)
+| ೈ፝͜͡🎭│${prefix}Jogo13 (Car Driving School Simulator 
+| ೈ፝͜͡🎭│${prefix}Jogo14 (Cyber Fighters Premium)
+| ೈ፝͜͡🎭│${prefix}Jogo15 (Drag Racing)
+| ೈ፝͜͡🎭│${prefix}Jogo16 (Mr. Brow)
+| ೈ፝͜͡🎭│${prefix}Jogo17 (Airport City)
+| ೈ፝͜͡🎭│${prefix}Jogo18 (Crash Bandicoot)
+| ೈ፝͜͡🎭│${prefix}Jogo19 (Real Steel World Robot Boxing 
+| ೈ፝͜͡🎭│${prefix}Jogo20 (Anger of Sticker)
+| ೈ፝͜͡🎭│${prefix}Jogo21 (Angry Birds 2 - APK+OBB)
+| ೈ፝͜͡🎭│${prefix}Jogo22 (Especial Force Group 2 - CS)
+| ೈ፝͜͡🎭│${prefix}Jogo23 (Roblox)
+| ೈ፝͜͡🎭│${prefix}Jogo24 (Critical Striker)
+| ೈ፝͜͡🎭│${prefix}Jogo25 (Dream League Soccer)
+| ೈ፝͜͡🎭│${prefix}Jogo26 (Clash of Clans)
+| ೈ፝͜͡🎭│${prefix}Jogo27 (Criminal Case Paris)
+| ೈ፝͜͡🎭│${prefix}Jogo28 (Drift Max World)
+| ೈ፝͜͡🎭│${prefix}Jogo29 (Downhill Masters)
+| ೈ፝͜͡🎭│${prefix}Jogo30 (Monopoly)
+╰════════════════════╯
+╭───────────────╮
+│     🔗𝐒𝐈𝐓𝐄𝐒🌐
+╰───────────────╯
+╭════════════════════╮
+| ೈ፝͜͡🌐│${prefix}site1 (modyolo)
+| ೈ፝͜͡🌐│${prefix}site2 (liteapks)
+| ೈ፝͜͡🌐│${prefix}site3 (getmods)
+| ೈ፝͜͡🌐│${prefix}site4 (apkmody)
+| ೈ፝͜͡🌐│${prefix}site5 (apkdone)
+| ೈ፝͜͡🎦│${prefix}site6 (pobreflix)
+| ೈ፝͜͡🎦│${prefix}site7 (megaflix)
+| ೈ፝͜͡🎦│${prefix}site8 (boraflix)
+| ೈ፝͜͡🎦│${prefix}site9 (superflix)
+| ೈ፝͜͡🎦│${prefix}site10 (filmesxp)
+| ೈ፝͜͡📺│${prefix}site11 (multicanais)
+| ೈ፝͜͡📺│${prefix}site12 (futemax)
+| ೈ፝͜͡📺│${prefix}site13 (redecanais)
+| ೈ፝͜͡📺│${prefix}site14 (player tv)
+| ೈ፝͜͡📺│${prefix}site15 (pirate tv)
+╰════════════════════╯`
+}
+
+exports.menuent = menuent
