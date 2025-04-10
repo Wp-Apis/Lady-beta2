c9cd6ed209a6ed514ca46635d8272e31fee64b42
Luanzindesenvolve <164371570+Luanzindesenvolve@users.noreply.github.com>
2025-04-10 16:56:03 -0300

    Add files via upload

---
+++ dono/menus/pesquisas.js
@@ -0,0 +1,52 @@
+const menupes = (sender, prefix) => {
+return `༒BEM-VINDO༒
+⇒ @${sender.split("@")[0]}
+
+╭───────────────╮
+│   🔎𝐏𝐄𝐒𝐐𝐔𝐈𝐒𝐀𝐒🕵️‍♂️
+╰───────────────╯
+╭────────────────╮
+| ೈ፝͜͡🔎│${prefix}tabela_brasileirao
+| ೈ፝͜͡🔎│${prefix}tabela_ucl
+| ೈ፝͜͡🔎│${prefix}tabela_premierleague
+| ೈ፝͜͡🔎│${prefix}tabela_laliga
+| ೈ፝͜͡🔎│${prefix}tabela_bundesliga
+| ೈ፝͜͡🔎│${prefix}tabela_seriea
+| ೈ፝͜͡🔎│${prefix}tabela_ligue1
+| ೈ፝͜͡🔎│${prefix}tabela_portugal
+| ೈ፝͜͡🔎│${prefix}guia-filmes
+| ೈ፝͜͡🔎│${prefix}guia-series
+| ೈ፝͜͡🔎│${prefix}guia-esportes
+| ೈ፝͜͡🔎│${prefix}guia-infantil
+| ೈ፝͜͡🔎│${prefix}guia-variedades
+| ೈ፝͜͡🔎│${prefix}guia-documentarios
+| ೈ፝͜͡🔎│${prefix}guia-noticias
+| ೈ፝͜͡🔎│${prefix}guia-aberta
+| ೈ፝͜͡🔎│${prefix}jogador 
+| ೈ፝͜͡🔎│${prefix}tv
+| ೈ፝͜͡🔎│${prefix}tvfutebol
+| ೈ፝͜͡🔎│${prefix}jogosdodia
+| ೈ፝͜͡🔎│${prefix}ucl 
+| ೈ፝͜͡🔎│${prefix}tvbrasileirao
+| ೈ፝͜͡🔎│${prefix}nfl
+| ೈ፝͜͡🔎│${prefix}basquete 
+| ೈ፝͜͡🔎│${prefix}ufc
+| ೈ፝͜͡🔎│${prefix}esportedodia
+| ೈ፝͜͡🔎│${prefix}filme (nome)
+| ೈ፝͜͡🔎│${prefix}serie (nome)
+| ೈ፝͜͡🔎│${prefix}cotacao (moeda)
+| ೈ፝͜͡🔎│${prefix}livro (nome)
+| ೈ፝͜͡🔎│${prefix}celular (modelo)
+| ೈ፝͜͡🔎│${prefix}letra (musica)
+| ೈ፝͜͡🔎│${prefix}verificarban (id ff)
+| ೈ፝͜͡🔎│${prefix}wikipedia (assunto)
+| ೈ፝͜͡🔎│${prefix}pinterest (titulo)
+| ೈ፝͜͡🔎│${prefix}clima (cidade)
+| ೈ፝͜͡🔎│${prefix}temporal (cidade)
+| ೈ፝͜͡🔎│${prefix}biblia (versículo)
+| ೈ፝͜͡🔎│${prefix}receita (comida em inglês)
+| ೈ፝͜͡🔎│${prefix}significado (nome)
+╰─────────────────`
+}
+
+exports.menupes = menupes
