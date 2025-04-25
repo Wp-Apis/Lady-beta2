62c15a2f5820f3fe916374b11d637f1bf06c5b2a
Luanzindesenvolve <164371570+Luanzindesenvolve@users.noreply.github.com>
2025-04-25 17:49:32 -0300

    Add files via upload

---
+++ dono/menus/wallpapers.js
@@ -0,0 +1,118 @@
+const menuwal = (sender, prefix) => {
+return `༒BEM-VINDO༒
+⇒ @${sender.split("@")[0]}
+
+╭───────────────╮
+│ 🖼️ 𝐖𝐀𝐋𝐋𝐏𝐀𝐏𝐄𝐑𝐒 📷
+╰───────────────╯
+╭════════════════════╮
+| ೈ፝͜͡📷│${prefix}gatos
+| ೈ፝͜͡📷│${prefix}cachorros
+| ೈ፝͜͡📷│${prefix}cosplay
+| ೈ፝͜͡📷│${prefix}cosplayloli
+| ೈ፝͜͡📷│${prefix}cosplaysagiri
+| ೈ፝͜͡📷│${prefix}fotodev
+| ೈ፝͜͡📷│${prefix}gameWallp
+| ೈ፝͜͡📷│${prefix}wallpaper_aleatorio
+| ೈ፝͜͡📷│${prefix}meme
+| ೈ፝͜͡📷│${prefix}pokemon
+| ೈ፝͜͡📷│${prefix}satanic
+| ೈ፝͜͡📷│${prefix}nime
+| ೈ፝͜͡📷│${prefix}wallhp2
+| ೈ፝͜͡📷│${prefix}Montanhas
+| ೈ፝͜͡📷│${prefix}Motos
+| ೈ፝͜͡📷│${prefix}carros
+| ೈ፝͜͡📷│${prefix}hakcers
+| ೈ፝͜͡📷│${prefix}cartun
+| ೈ፝͜͡📷│${prefix}Cats
+| ೈ፝͜͡📷│${prefix}pets
+| ೈ፝͜͡📷│${prefix}Programador
+
+╭───────────────╮
+│ 🖼️ 𝐅𝐄𝐌𝐈𝐍𝐈𝐍𝐎 📷
+╰───────────────╯
+╭════════════════════╮
+| ೈ፝͜͡📷│${prefix}elaina
+| ೈ፝͜͡📷│${prefix}emilia
+| ೈ፝͜͡📷│${prefix}hinata
+| ೈ፝͜͡📷│${prefix}iconfeminino
+| ೈ፝͜͡📷│${prefix}mikasa
+| ೈ፝͜͡📷│${prefix}rose
+| ೈ፝͜͡📷│${prefix}Rize
+| ೈ፝͜͡📷│${prefix}nezuko
+| ೈ፝͜͡📷│${prefix}sakura
+| ೈ፝͜͡📷│${prefix}sagiri
+| ೈ፝͜͡📷│${prefix}tsunade
+| ೈ፝͜͡📷│${prefix}waifu
+| ೈ፝͜͡📷│${prefix}waifu2
+| ೈ፝͜͡📷│${prefix}akiyama
+| ೈ፝͜͡📷│${prefix}anaicon
+| ೈ፝͜͡📷│${prefix}asuna
+| ೈ፝͜͡📷│${prefix}ayuzawa
+| ೈ፝͜͡📷│${prefix}chitoge
+| ೈ፝͜͡📷│${prefix}ebaicon
+| ೈ፝͜͡📷│${prefix}lisakpop
+| ೈ፝͜͡📷│${prefix}gremory
+| ೈ፝͜͡📷│${prefix}hestia
+| ೈ፝͜͡📷│${prefix}isuzu
+| ೈ፝͜͡📷│${prefix}jiso
+| ೈ፝͜͡📷│${prefix}lisakpop
+| ೈ፝͜͡📷│${prefix}itori
+| ೈ፝͜͡📷│${prefix}kotori
+| ೈ፝͜͡📷│${prefix}Kpop
+| ೈ፝͜͡📷│${prefix}ryujin
+| ೈ፝͜͡📷│${prefix}justina
+| ೈ፝͜͡📷│${prefix}jeni
+| ೈ፝͜͡📷│${prefix}Kaori
+| ೈ፝͜͡📷│${prefix}Kagura
+| ೈ፝͜͡📷│${prefix}Miku
+| ೈ፝͜͡📷│${prefix}Kurumi
+| ೈ፝͜͡📷│${prefix}Megumin
+| ೈ፝͜͡📷│${prefix}shinomiya
+| ೈ፝͜͡📷│${prefix}toukachan
+| ೈ፝͜͡📷│${prefix}profil
+| ೈ፝͜͡📷│${prefix}pentol
+| ೈ፝͜͡📷│${prefix}nekonime
+| ೈ፝͜͡📷│${prefix}tejina
+| ೈ፝͜͡📷│${prefix}japinhas
+| ೈ፝͜͡📷│${prefix}shina
+| ೈ፝͜͡📷│${prefix}shinka
+| ೈ፝͜͡📷│${prefix}yotsuba
+| ೈ፝͜͡📷│${prefix}shizuka
+| ೈ፝͜͡📷│${prefix}tatasurya
+
+╭───────────────╮
+│ 🖼️ 𝐌𝐀𝐒𝐂𝐔𝐋𝐈𝐍𝐎 📷
+╰───────────────╯
+╭════════════════════╮
+| ೈ፝͜͡📷│${prefix}Itachi
+| ೈ፝͜͡📷│${prefix}boruto
+| ೈ፝͜͡📷│${prefix}akira
+| ೈ፝͜͡📷│${prefix}chucky
+| ೈ፝͜͡📷│${prefix}deidara
+| ೈ፝͜͡📷│${prefix}sasuke
+| ೈ፝͜͡📷│${prefix}minato
+| ೈ፝͜͡📷│${prefix}Naruto
+| ೈ፝͜͡📷│${prefix}kakashi
+
+╭───────────────╮
+│ 🖼️ 𝐄𝐃𝐈𝐓𝐒 📷
+╰───────────────╯
+╭════════════════════╮
+
+| ೈ፝͜͡📷│${prefix}lomotifs
+| ೈ፝͜͡📷│${prefix}chainsaw
+| ೈ፝͜͡📷│${prefix}narutoedits
+| ೈ፝͜͡📷│${prefix}kimetsu
+| ೈ፝͜͡📷│${prefix}bleach
+| ೈ፝͜͡📷│${prefix}jujutsu
+| ೈ፝͜͡📷│${prefix}Editaleatoria
+| ೈ፝͜͡📷│${prefix}hunterx
+| ೈ፝͜͡📷│${prefix}soloedits
+| ೈ፝͜͡📷│${prefix}animedits
+| ೈ፝͜͡📷│${prefix}futeboledits
+╰════════════════════╯
+`
+}
+
+exports.menuwal = menuwal
