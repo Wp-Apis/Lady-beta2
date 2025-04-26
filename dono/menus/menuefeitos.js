317eb5ad8b5569e033c37b35b4638dcc3aa01b47
Luanzindesenvolve <164371570+Luanzindesenvolve@users.noreply.github.com>
2025-04-26 16:59:26 -0300

    Add files via upload

---
+++ dono/menus/menuefeitos.js
@@ -0,0 +1,44 @@
+const menuefeitos = (sender, prefix) => {
+return `༒W̷E̷L̷C̷O̷M̷E̷༒
+⇒ @${sender.split('@')[0]}
+
+『 𝐌𝐄𝐍𝐔✰𝐄𝐅𝐄𝐈𝐓𝐎𝐒 』
+╭════════════════════╯
+ | ೈ፝͜͡🧩 ${prefix}editroxo
+ | ೈ፝͜͡🧩 ${prefix}editvermelha
+ | ೈ፝͜͡🧩 ${prefix}editbranca
+ | ೈ፝͜͡🧩 ${prefix}perfilzap
+ | ೈ፝͜͡🧩 ${prefix}vasco-edit
+ | ೈ፝͜͡🧩 ${prefix}adorartv
+ | ೈ፝͜͡🧩 ${prefix}interro
+ | ೈ፝͜͡🧩 ${prefix}brasil
+ | ೈ፝͜͡🧩 ${prefix}lolifofaa
+ | ೈ፝͜͡🧩 ${prefix}lolifofa
+ | ೈ፝͜͡🧩 ${prefix}carinho
+ | ೈ፝͜͡🧩 ${prefix}lolinopc
+ | ೈ፝͜͡🧩 ${prefix}cartoon
+ | ೈ፝͜͡🧩 ${prefix}toanime
+ | ೈ፝͜͡🧩 ${prefix}tozombie
+ | ೈ፝͜͡🧩 ${prefix}remini
+ | ೈ፝͜͡🧩 ${prefix}affect
+ | ೈ፝͜͡🧩 ${prefix}comunismo
+ | ೈ፝͜͡🧩 ${prefix}mms
+ | ೈ፝͜͡🧩 ${prefix}bobross
+ | ೈ፝͜͡🧩 ${prefix}bolsonaro
+ | ೈ፝͜͡🧩 ${prefix}blur
+ | ೈ፝͜͡🧩 ${prefix}beautiful
+ | ೈ፝͜͡🧩 ${prefix}circle
+ | ೈ፝͜͡🧩 ${prefix}del
+ | ೈ፝͜͡🧩 ${prefix}invert
+ | ೈ፝͜͡🧩 ${prefix}facepalm
+ | ೈ፝͜͡🧩 ${prefix}rip
+ | ೈ፝͜͡🧩 ${prefix}wasted
+ | ೈ፝͜͡🧩 ${prefix}wanted
+ | ೈ፝͜͡🧩 ${prefix}trash
+ | ೈ፝͜͡🧩 ${prefix}sepia
+ | ೈ፝͜͡🧩 ${prefix}pixelate
+ | ೈ፝͜͡🧩 ${prefix}lgbt
+╰════════════════════╮`
+}
+
+exports.menuefeitos = menuefeitos
---
+++ dono/menus/menuia.js
@@ -0,0 +1,88 @@
+const menuia = (sender, prefix) => {
+return `༒BEM-VINDO༒
+⇒ @${sender.split("@")[0]}
+╭───────────────╮
+│   🧠𝐌𝐄𝐍𝐔 𝐈𝐀𝐒🤖
+╰───────────────╯
+╭════════════════════╮
+ | ೈ፝͜͡🤖 ${prefix}chatgpt
+ | ೈ፝͜͡🤖 ${prefix}gpt
+ | ೈ፝͜͡🤖 ${prefix}gpt4
+ | ೈ፝͜͡🤖 ${prefix}chatgpt4
+ | ೈ፝͜͡🤖 ${prefix}chatgpt3
+ | ೈ፝͜͡🤖 ${prefix}chatgpt4-o
+ | ೈ፝͜͡🤖 ${prefix}gemini
+ | ೈ፝͜͡🤖 ${prefix}geminipro
+ | ೈ፝͜͡🤖 ${prefix}lady
+ | ೈ፝͜͡🤖 ${prefix}sabetudo
+ | ೈ፝͜͡🤖 ${prefix}ia
+ | ೈ፝͜͡🤖 ${prefix}llama
+ | ೈ፝͜͡🤖 ${prefix}simsimi
+ | ೈ፝͜͡🤖 ${prefix}blackbox
+ | ೈ፝͜͡🤖 ${prefix}lumin
+ | ೈ፝͜͡🤖 ${prefix}letgpt
+ | ೈ፝͜͡🤖 ${prefix}wwdgtp
+ | ೈ፝͜͡🤖 ${prefix}grok  
+ | ೈ፝͜͡🤖 ${prefix}Grok  
+ | ೈ፝͜͡🤖 ${prefix}gpt4o 
+ | ೈ፝͜͡🤖 ${prefix}gpt4turbo    
+ | ೈ፝͜͡🤖 ${prefix}claude3opus  
+ | ೈ፝͜͡🤖 ${prefix}grokbeta  
+ | ೈ፝͜͡🤖 ${prefix}grok2mini  
+ | ೈ፝͜͡🤖 ${prefix}claude3sonnet    
+ | ೈ፝͜͡🤖 ${prefix}claude35sonnet   
+ | ೈ፝͜͡🤖 ${prefix}claude35sonnet2   
+ | ೈ፝͜͡🤖 ${prefix}dalle  
+ | ೈ፝͜͡🤖 ${prefix}lexica
+ | ೈ፝͜͡🤖 ${prefix}Mora
+ | ೈ፝͜͡🤖 ${prefix}qio
+ | ೈ፝͜͡🤖 ${prefix}Meta
+ | ೈ፝͜͡🤖 ${prefix}meta-turbo
+ | ೈ፝͜͡🤖 ${prefix}bard
+ | ೈ፝͜͡🤖 ${prefix}bard2
+ | ೈ፝͜͡🤖 ${prefix}blackboxai
+ | ೈ፝͜͡🤖 ${prefix}blackboxai-pro
+╰═══════════════════════╮
+╭═══════════════════════╯
+ | ೈ፝͜͡🤖 ${prefix}deepimage (128 modelos)
+ | ೈ፝͜͡🤖 ${prefix}animarfoto (foto vira desenho)
+ | ೈ፝͜͡🤖 ${prefix}expandirfoto (aguenta o tamanho)
+ | ೈ፝͜͡🤖 ${prefix}melhorarfoto (aumenta pixels)
+ | ೈ፝͜͡🤖 ${prefix}colorir-foto (dar cor na foto)
+ | ೈ፝͜͡🤖 ${prefix}escreverlivro (escreve no livro)
+ | ೈ፝͜͡🤖 ${prefix}gerarqrcode (cria um qrcode)
+ | ೈ፝͜͡🤖 ${prefix}tirarfundo (tira o fundo)
+ | ೈ፝͜͡🤖 ${prefix}removermarca (tira marca)
+ | ೈ፝͜͡🤖 ${prefix}explicarfoto (explica a foto)
+ | ೈ፝͜͡🤖 ${prefix}image2text (explica a imagem)
+ | ೈ፝͜͡🤖 ${prefix}trocar-rosto
+ | ೈ፝͜͡🤖 ${prefix}gerararte
+ | ೈ፝͜͡🤖 ${prefix}ximage
+ | ೈ፝͜͡🤖 ${prefix}magicstudio
+ | ೈ፝͜͡🤖 ${prefix}flux-anime
+ | ೈ፝͜͡🤖 ${prefix}flux-fantasy
+ | ೈ፝͜͡🤖 ${prefix}flux-digital
+ | ೈ፝͜͡🤖 ${prefix}flux-neon_punk
+ | ೈ፝͜͡🤖 ${prefix}flux-dark
+ | ೈ፝͜͡🤖 ${prefix}flux-low_poly
+ | ೈ፝͜͡🤖 ${prefix}flux-line_art
+ | ೈ፝͜͡🤖 ${prefix}flux-pixel_art
+ | ೈ፝͜͡🤖 ${prefix}flux-comic
+ | ೈ፝͜͡🤖 ${prefix}flux-analog_film
+ | ೈ፝͜͡🤖 ${prefix}flux-surreal
+ | ೈ፝͜͡🤖 ${prefix}fluximg
+ | ೈ፝͜͡🤖 ${prefix}amazonai
+ | ೈ፝͜͡🤖 ${prefix}animagine
+ | ೈ፝͜͡🤖 ${prefix}flux
+ | ೈ፝͜͡🤖 ${prefix}gerar-foto
+ | ೈ፝͜͡🤖 ${prefix}imagine
+ | ೈ፝͜͡🤖 ${prefix}imagine2
+ | ೈ፝͜͡🤖 ${prefix}imagine3
+ | ೈ፝͜͡🤖 ${prefix}fluximg
+ | ೈ፝͜͡🤖 ${prefix}sd 
+ | ೈ፝͜͡🤖 ${prefix}imgsys
+ | ೈ፝͜͡🤖 ${prefix}orbital-img
+╰════════════════════╯`
+}
+
+exports.menuia = menuia
