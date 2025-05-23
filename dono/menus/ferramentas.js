b1e778c8d2e0472c45e90aeafdc0d943276912e9
Luanzindesenvolve <164371570+Luanzindesenvolve@users.noreply.github.com>
2025-05-22 21:55:46 -0300

    Add files via upload

---
+++ dono/menus/ferramentas.js
@@ -0,0 +1,61 @@
+const menufer = (sender, prefix) => {
+return `༒BEM-VINDO༒
+⇒ @${sender.split("@")[0]}
+╭───────────────╮
+│⚒️ 𝑭𝑬𝑹𝑹𝑨𝑴𝑬𝑵𝑻𝑨𝑺 ⚒️
+╰───────────────╯
+╭════════════════════╮
+| ೈ፝͜͡⚒️│${prefix}fakemail (email temp)
+| ೈ፝͜͡⚒️│${prefix}fakemail-msg (msg de email)
+| ೈ፝͜͡⚒️│${prefix}numerofake-paises
+| ೈ፝͜͡⚒️│${prefix}numerofake-lista
+| ೈ፝͜͡⚒️│${prefix}numerofake-msg
+| ೈ፝͜͡⚒️│${prefix}dnscheck
+| ೈ፝͜͡⚒️│${prefix}tcpcheck
+| ೈ፝͜͡⚒️│${prefix}chekhost
+| ೈ፝͜͡⚒️│${prefix}codificar
+| ೈ፝͜͡⚒️│${prefix}decodificar
+| ೈ፝͜͡⚒️│${prefix}printsite
+| ೈ፝͜͡⚒️│${prefix}dicionario
+| ೈ፝͜͡⚒️│${prefix}morse
+| ೈ፝͜͡⚒️│${prefix}linkimg
+| ೈ፝͜͡⚒️│${prefix}linkimg2
+| ೈ፝͜͡⚒️│${prefix}linkimg3
+| ೈ፝͜͡⚒️│${prefix}linkvideo
+| ೈ፝͜͡⚒️│${prefix}linkvideo2
+| ೈ፝͜͡⚒️│${prefix}linkaudio
+| ೈ፝͜͡⚒️│${prefix}linkaudio2
+| ೈ፝͜͡⚒️│${prefix}linkfoto
+| ೈ፝͜͡⚒️│${prefix}linkmidia
+| ೈ፝͜͡⚒️│${prefix}linkmidia2
+| ೈ፝͜͡⚒️│${prefix}fazerlink
+| ೈ፝͜͡⚒️│${prefix}fazerlink2
+| ೈ፝͜͡⚒️│${prefix}transcrever
+| ೈ፝͜͡⚒️│${prefix}transcrição
+| ೈ፝͜͡⚒️│${prefix}audiomeme (baixa)
+| ೈ፝͜͡⚒️│${prefix}pesqaudio (lista audios meme)
+| ೈ፝͜͡⚒️│${prefix}fazernick
+| ೈ፝͜͡⚒️│${prefix}frases
+| ೈ፝͜͡⚒️│${prefix}pensador
+| ೈ፝͜͡⚒️│${prefix}signo
+| ೈ፝͜͡⚒️│${prefix}html (ver html)
+| ೈ፝͜͡⚒️│${prefix}traduzir
+| ೈ፝͜͡⚒️│${prefix}encutarlink
+| ೈ፝͜͡⚒️│${prefix}linkcurto
+| ೈ፝͜͡⚒️│${prefix}adfoc (cria link)
+| ೈ፝͜͡⚒️│${prefix}botly (cria link?
+| ೈ፝͜͡⚒️│${prefix}ouo (cria link)
+| ೈ፝͜͡⚒️│${prefix}cleanuri (cria link)
+| ೈ፝͜͡⚒️│${prefix}ssurl
+| ೈ፝͜͡⚒️│${prefix}shortlink
+| ೈ፝͜͡⚒️│${prefix}proximojogo
+| ೈ፝͜͡⚒️│${prefix}rastrearshop
+| ೈ፝͜͡⚒️│${prefix}likeff
+| ೈ፝͜͡⚒️│${prefix}info-ff
+| ೈ፝͜͡⚒️│${prefix}check-ban
+| ೈ፝͜͡⚒️│${prefix}seguidores
+╰════════════════════╯
+` 
+}
+
+exports.menufer = menufer
